8c6d8ad56ec25958a1efc6ecc164b4070fbf99fa
santoshraja <santoshraja.d@gmail.com>
2016-05-09 17:37:43 +0800

    22

--- target/classes/com/smbc/step/plugin/WebContent/DatePlugin.js
+++ target/classes/com/smbc/step/plugin/WebContent/DatePlugin.js
@@ -9,7 +9,8 @@ function(declare, connect,  Desktop, ValueFormatter) {
 		return;
 	}
 	//hi it update to git hub????
-	
+	// is it connected to git hub ??????//????
+	console.log("is it connected to git hub ??????//???? ???? 122232323 343443");
 	 Desktop.valueFormatter = new ValueFormatter();
 	connect.connect(ecm.model.desktop, "onLogin", function() {
 		console.log("updating with icm.util.Util only for icm desktop");
